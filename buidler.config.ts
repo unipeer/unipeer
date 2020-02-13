42db24a10919fbab5348d5ff0de80a300390b0f7
Shaleen Jain <shaleen@jain.sh>
2020-02-13 21:04:49 +0530

    initialize project with buidler.dev and typescript

---
+++ buidler.config.ts
@@ -0,0 +1,31 @@
+import { task, usePlugin } from "@nomiclabs/buidler/config";
+import { BuidlerConfig } from "@nomiclabs/buidler/config";
+
+usePlugin("@nomiclabs/buidler-truffle5");
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
+  const accounts = await bre.web3.eth.getAccounts();
+
+  for (const account of accounts) {
+    console.log(account);
+  }
+});
+
+const config: BuidlerConfig = {
+  defaultNetwork: "buidlerevm",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    buidlerevm: {
+      // See its defaults
+    }
+  },
+  solc: {
+    version: "0.5.15",
+  }
+};
+
+export default config;
