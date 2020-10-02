89aeac73e54c58e26b9b624170d5619b52182043
Shaleen Jain <shaleen@jain.sh>
2020-10-02 16:16:58 +0530

    add sample deploy script

---
+++ scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { run, ethers, upgrades} from "@nomiclabs/buidler";
+
+async function main() {
+  // You can run Buidler tasks from a script.
+  // For example, we make sure everything is compiled by running "compile"
+  await run("compile");
+
+  const Box = await ethers.getContractFactory("Box");
+  console.log("Deploying Box...");
+
+  const box = await upgrades.deployProxy(Box, [42], { initializer: 'store' });
+  console.log("Box deployed to:", box.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
