d237e9101600d9708d535c9fd207d9f03af73443
Shaleen Jain <shaleen@jain.sh>
2020-10-06 11:33:27 +0530

    add frontend code

---
+++ src/pages/index.js
@@ -0,0 +1,87 @@
+import { verifyMessage } from "@ethersproject/wallet";
+import { useWeb3React } from "@web3-react/core";
+import Head from "next/head";
+import Link from "next/link";
+import Account from "../components/Account";
+import ETHBalance from "../components/ETHBalance";
+import useEagerConnect from "../hooks/useEagerConnect";
+import usePersonalSign, { hexlify } from "../hooks/usePersonalSign";
+
+export default function Home() {
+  const { account, library } = useWeb3React();
+
+  const triedToEagerConnect = useEagerConnect();
+
+  const sign = usePersonalSign();
+
+  const handleSign = async () => {
+    const msg = "Next Web3 Boilerplate Rules";
+    const sig = await sign(msg);
+    console.log(sig);
+    console.log("isValid", verifyMessage(msg, sig) === account);
+  };
+
+  const isConnected = typeof account === "string" && !!library;
+
+  return (
+    <div>
+      <Head>
+        <title>Next Web3 Boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+
+          <Account triedToEagerConnect={triedToEagerConnect} />
+        </nav>
+      </header>
+
+      <main>
+        <h1>
+          Welcome
+        </h1>
+
+        {isConnected && (
+          <section>
+            <ETHBalance />
+            <button onClick={handleSign}>Personal Sign</button>
+          </section>
+        )}
+      </main>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          padding: 5px;
+          justify-content: space-between;
+        }
+
+        main {
+          text-align: center;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+        }
+
+        html {
+          font-family: sans-serif, Apple Color Emoji, Segoe UI Emoji,
+            Segoe UI Symbol, Noto Color Emoji;
+          line-height: 1.5;
+        }
+
+        *,
+        *::after,
+        *::before {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
