2e18150e559b0a3de0bbe747a87f95ecbf08940e
Shaleen Jain <shaleen@jain.sh>
2022-06-06 11:36:46 +0000

    wip

--- contracts/src/Unipeer.sol
+++ contracts/src/Unipeer.sol
@@ -3,13 +3,17 @@ pragma solidity 0.8.10;
 
 contract Unipeer {
 
+    address public admin;
+
     struct PaymentMethod {
+        uint8 metaEvidenceId;
         string paymentName;
         bytes extraData; // subcourtId + minJurors;
-        uint8 metaEvidenceId;
         mapping(address => bool) tokenWhitelist;
+        // address[] tokens;
     }
-    PaymentMethod[] public paymentMethods;
+    uint16 public numOfMethods;
+    mapping(uint => PaymentMethod) paymentMethods;
 
     // paymentAddressBySellerPaymentId[seller][paymentId] = paymentAddress
     mapping(address => mapping(uint16 => string)) paymentAddressBySellerPaymentId;
@@ -23,4 +27,35 @@ contract Unipeer {
     mapping(address => mapping(uint16 => address[])) sellerByTokenPaymentId;
     mapping(address => mapping(uint16 => uint256)) indexCounter;
 
+    event NewPaymentMethod(uint16 methodId, string paymentName);
+
+    modifier onlyAdmin() {
+        require(admin == msg.sender, "Access not allowed: Admin only.");
+        _;
+    }
+
+    function addPaymentMethod(
+        uint8 _metaEvidenceId,
+        string calldata _paymentName,
+        bytes calldata _extraData,
+        address[] calldata _tokens
+    ) public onlyAdmin {
+        PaymentMethod storage pm = paymentMethods[numOfMethods++];
+        pm.metaEvidenceId = _metaEvidenceId;
+        pm.paymentName = _paymentName;
+        pm.extraData = _extraData;
+        for (uint256 i = 0 ; i < _tokens.length; i++) {
+            pm.tokenWhitelist[_tokens[i]] = true;
+        }
+
+        emit NewPaymentMethod(numOfMethods - 1, _paymentName);
+    }
+
+    function addPaymentToken(uint16 paymentId, address token) public {
+
+    }
+
+    function updatePaymentAddress(uint16 paymentId, string calldata paymentAddress) public {
+        require(paymentId < numOfMethods);
+    }
 }
